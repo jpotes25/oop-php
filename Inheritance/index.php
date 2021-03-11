1ca20c1ccfaff577cfd52c31a5d05376a1ea9e28
Potesoffice <javierpotests@gmail.com>
2021-03-11 13:25:48 -0500

    finished inheritance, some tips on index.php

--- Inheritance/index.php
+++ Inheritance/index.php
@@ -15,10 +15,13 @@ class User
     }
 }
 
-
+/**
+ * We can use inheritance to take the info of one class to another.
+ * We can't use inheritance if the class is a final class
+ */
 class Admin extends User
 {
-    //..
+    //.. we can overwrite superclass methods, only if that method is not "final"
 }
 
 $admin = new Admin('David');
